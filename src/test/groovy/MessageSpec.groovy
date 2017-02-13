06d2ebc8222deb5d1adc31780a61126cc83bf7ea
Wes Lloyd-laptop <wlloyd@acm.org>
2017-02-12 17:02:26 -0800

    added mock tests

--- src/test/groovy/MessageSpec.groovy
+++ src/test/groovy/MessageSpec.groovy
@@ -3,13 +3,16 @@ import objects.*;
 class MessageSpec extends spock.lang.Specification  {
   def "message.setMessage"() {
     given:
-    //def message = Mock(Message)
+    def message = Mock(Message)
+    1 * message.setMessage("Hello Fred") >> null
+    1 * message.getMessage() >> "Hello Fred"
 
     when: "set text of the message"
-    //message.setMessage("Hello Fred")
+    message.setMessage("Hello Fred")
 
     then: "expect text of the message"
-    //message.getMessage() == "Hello Fred"
+    message.getMessage() == "Hello Fred"
+
   }
 }
 
