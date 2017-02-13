17dacfcdffc991c5055ecf7f55573f1e08358fea
Wes Lloyd-laptop <wlloyd@acm.org>
2017-02-12 17:09:40 -0800

    more tests

---
+++ src/test/groovy/ModelSpecMock.groovy
@@ -0,0 +1,56 @@
+import data.*;
+import objects.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+class MockModelSpec extends spock.lang.Specification {
+  
+  def "mockmodelspec.newUserMock"() {
+   
+    given:
+    def userid = 0
+    def resultSet = makeResultSet(
+    ["userid", "name"  , "age"    ],
+    [  1 , "Fred", 35]
+    )    
+    def user = new User();
+    def mockConnection = Mock(Connection)
+    def mockStatement = Mock(Statement)
+    
+    // Number refers to the number of expected calls to the method
+    // If the actual number does not match, the test will fail
+    1 * mockConnection.createStatement() >> mockStatement
+    1 * mockStatement.execute(_ as String, 1) >> true
+    1 * mockConnection.isClosed() >> false
+    1 * mockStatement.getGeneratedKeys() >> resultSet
+    //1 * resultSet.getInt(3) << 1
+    user.setName("Jake Smith")
+    user.setAge(42)
+    System.setProperty "DBCONN", "jdbc:postgresql://ec2-54-235-204-221.compute-1.amazonaws.com:5432/d19m0j1erhvr7v?user=wxojhmodfpbmsv&password=80cfef5defecd78ff44e5e2bab48a26b06f930d1f57e097a6be957be98358c53&sslmode=require"
+    
+    when: "test create of new user"
+    //userid = Model.Singleton().newUser(user)
+    userid = Model.mockSingleton(mockConnection).newUser(user)
+
+    then: "check new user id"
+    userid > 0
+    //Model.singleton().deleteUser(userid)  
+    
+  }
+  
+  def cleanup() {}
+  def cleanupSpec() {}
+    
+  private ResultSet makeResultSet(List<String> aColumns, List... rows) {
+    def result = Mock(ResultSet)
+    int currentIndex = -1  
+ 
+    result.next() >> { ++currentIndex < rows.length }
+    result./get(String|Short|Date|Timestamp)/(_) >> { String argument ->
+    rows[currentIndex][aColumns.indexOf(argument)] } 
+    result./get(Int)/(3) >> 1    
+    return result
+  }    
+ 
+}
---
+++ src/test/java/objects/UserTest.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objects;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wlloyd
+ */
+public class UserTest {
+    
+    public UserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getName method, of class User.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        User instance = new User();
+        String expResult = null;
+        String result = instance.getName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setName method, of class User.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "Fred";
+        User instance = new User();
+        instance.setName(name);
+        String result = instance.getName();
+        assertEquals(name, result);
+    }
+
+    /**
+     * Test of getAge method, of class User.
+     */
+    @Test
+    public void testGetAge() {
+        System.out.println("getAge");
+        User instance = new User();
+        int expResult = 0;
+        int result = instance.getAge();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setAge method, of class User.
+     */
+    @Test
+    public void testSetAge() {
+        System.out.println("setAge");
+        int age = 35;
+        User instance = new User();
+        instance.setAge(age);
+        int result = instance.getAge();
+        assertEquals(age, result);
+    }
+
+    /**
+     * Test of getMessages method, of class User.
+     */
+    @Test
+    public void testGetMessages() {
+        System.out.println("getMessages");
+        User instance = new User();
+        List<String> expResult = null;
+        List<String> result = instance.getMessages();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setMessages method, of class User.
+     */
+    @Test
+    public void testSetMessages() {
+        System.out.println("setMessages");
+        List<String> messages = new LinkedList<String>();
+        messages.add("Hello Fred");
+        messages.add("What time is it?");
+        User instance = new User();
+        instance.setMessages(messages);
+        List<String> result = instance.getMessages();
+        assertEquals(messages.size(), result.size());
+        assertEquals(messages.get(0), result.get(0));
+        assertEquals(messages.get(1), result.get(1));
+    }
+
+    /**
+     * Test of getUserid method, of class User.
+     */
+    @Test
+    public void testGetUserid() {
+        System.out.println("getUserid");
+        User instance = new User();
+        int expResult = 0;
+        int result = instance.getUserid();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setUserId method, of class User.
+     */
+    @Test
+    public void testSetUserId() {
+        System.out.println("setUserId");
+        int userid = 10;
+        User instance = new User();
+        instance.setUserId(userid);
+        int result = instance.getUserid();
+        assertEquals(userid, result);
+    }
+    
+}
