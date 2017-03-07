b053b7234c706ae3e49cbed1e93c239db466c093
xutedin253 <thomasxu23@gmail.com>
2017-03-07 01:07:23 -0800

    bleh bleh

---
+++ src/main/java/com/mycompany/sample_maven_web_app/MessageService.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sample_maven_web_app;
+
+import static com.mycompany.sample_maven_web_app.UserService.logger;
+import data.Model;
+import data.ModelMessages;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import objects.Message;
+import objects.User;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ *
+ * @author wlloyd
+ */
+@Path("Messages")
+public class MessageService {
+    
+    static final Logger logger = Logger.getLogger(MessageService.class.getName());
+
+    public MessageService() 
+    {
+    }
+    
+    @GET
+    @Path("{messageid}")
+    @Produces(MediaType.TEXT_HTML)
+    public List<Message> getMessages(@PathParam("messageid") String id) {
+        LinkedList<Message> messageList = new LinkedList<Message>();
+     
+        try
+        {
+            int messageid = Integer.parseInt(id);
+            ModelMessages db = ModelMessages.singleton();
+            Message[] messages = db.getMessages(messageid);
+            if (messageid == 0)
+                for (int i=0;i<messages.length;i++)
+                    messageList.add(messages[i]);
+            else
+                messageList.add(messages[0]);
+            logger.log(Level.INFO, "Received request to fetch user id=" + messageid);
+            return messageList;
+        }
+        catch (Exception e)
+        {
+            JSONObject obj = new JSONObject();
+                logger.log(Level.WARNING, "Error getting users:" + e.toString());
+                return null;
+        }
+        
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("<html><body><style>table, th, td {font-family:Arial,Verdana,sans-serif;font-size:16px;padding: 0px;border-spacing: 0px;}</style><b>MESSAGES LIST:</b><br><br><table cellpadding=10 border=1><tr><td>Message Id</td><td>User Id</td><td>Message</td><td>Date Added</td></tr>");
+//        try
+//        {
+//            ModelMessages db = ModelMessages.singleton();
+//            Message[] msgs = db.getMessages();
+//            for (int i=0;i<msgs.length;i++)
+//                sb.append("<tr><td>" + msgs[i].getMessageId()+ "</td><td>" + msgs[i].getUserId()+ "</td><td>" + msgs[i].getMessage()+ "</td><td>" + msgs[i].getDateadded()+ "</td></tr>");
+//        }
+//        catch (Exception e)
+//        {
+//            sb.append("</table><br>Error getting messages: " + e.toString() + "<br>");
+//        }
+//        sb.append("</table></body></html>");
+//        return sb.toString();
+    }
+    
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String updateMessage(String jobj) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Message msg = mapper.readValue(jobj.toString(), Message.class);
+        StringBuilder text = new StringBuilder();
+        try {
+            ModelMessages db = ModelMessages.singleton();
+            int messageid = msg.getMessageId();
+            db.updateMessage(msg);
+            logger.log(Level.INFO, "update msg with messageid=" + messageid);
+            text.append("Message id updated with message id=" + messageid + "\n");
+        }
+        catch (SQLException sqle)
+        {
+            String errText = "Error updating user after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+            logger.log(Level.SEVERE, errText);
+            text.append(errText);
+        }
+        catch (Exception e)
+        {
+            logger.log(Level.SEVERE, "Error connecting to db.");
+            text.append("Error connecting to db.");
+        }
+        return text.toString();
+    }
+    
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String deleteMessage(String jobj) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Message msg = mapper.readValue(jobj.toString(), Message.class);
+        StringBuilder text = new StringBuilder();
+        try {
+            ModelMessages db = ModelMessages.singleton();
+            int messageid = msg.getMessageId();
+            db.deleteMessage(messageid);
+            logger.log(Level.INFO, "message deleted from db=" + messageid);
+            text.append("Message id deleted with id=" + messageid);
+        }
+        catch (SQLException sqle)
+        {
+            String errText = "Error deleteing msg after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+            logger.log(Level.SEVERE, errText);
+            text.append(errText);
+        }
+        catch (Exception e)
+        {
+            logger.log(Level.SEVERE, "Error connecting to db.");
+            text.append("Error connecting to db.");
+        }
+        return text.toString();
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String CreateMessage(String jobj) throws IOException 
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Message msg = mapper.readValue(jobj.toString(), Message.class);
+        StringBuilder text = new StringBuilder();
+        
+        try {
+            ModelMessages db = ModelMessages.singleton();
+            int msgid = db.newMessage(msg);
+            logger.log(Level.INFO, "msg persisted to db with msgid=" + msgid);
+            text.append("Msg id persisted with id=" + msgid);
+        }
+        catch (SQLException sqle)
+        {
+            String errText = "Error persisting user after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+            logger.log(Level.SEVERE, errText);
+            text.append(errText);
+        }
+        catch (Exception e)
+        {
+            logger.log(Level.SEVERE, "Error connecting to db.");
+        }
+        
+        return text.toString();
+    }
+    
+}
---
+++ src/main/java/data/ModelMessages.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import objects.Message;
+import objects.User;
+
+/**
+ *
+ * @author wlloyd
+ */
+public class ModelMessages {
+    static final Logger logger = Logger.getLogger(ModelMessages.class.getName());
+    private static ModelMessages instance;
+    private Connection conn;
+    
+    public static ModelMessages singleton() throws Exception {
+        if (instance == null) {
+            instance = new ModelMessages();
+        }
+        return instance;
+    }
+    
+    ModelMessages() throws Exception
+    {
+        Class.forName("org.postgresql.Driver");
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if ((dbUrl == null) || (dbUrl.length() < 1))
+            dbUrl = System.getProperties().getProperty("DBCONN");
+        logger.log(Level.INFO, "dbUrl=" + dbUrl);  
+        logger.log(Level.INFO, "attempting db connection");
+        conn = DriverManager.getConnection(dbUrl);
+        logger.log(Level.INFO, "db connection ok.");
+    }
+    
+    private Connection getConnection()
+    {
+        return conn;
+    }
+    
+    private Statement createStatement() throws SQLException
+    {
+        Connection conn = getConnection();
+        if ((conn != null) && (!conn.isClosed()))
+        {
+            logger.log(Level.INFO, "attempting statement create");
+            Statement st = conn.createStatement();
+            logger.log(Level.INFO, "statement created");
+            return st;
+        }
+        else
+        {
+            // Handle connection failure
+        }
+        return null;
+    }
+    
+    private PreparedStatement createPreparedStatement(String sql) throws SQLException
+    {
+        Connection conn = getConnection();
+        if ((conn != null) && (!conn.isClosed()))
+        {
+            logger.log(Level.INFO, "attempting statement create");
+            PreparedStatement pst = conn.prepareStatement(sql);
+            logger.log(Level.INFO, "prepared statement created");
+            return pst;
+        }
+        else
+        {
+            // Handle connection failure
+        }
+        return null;
+    }
+    
+    public Message[] getMessages(int messageId) throws SQLException
+    {
+        LinkedList<Message> ll = new LinkedList<Message>();
+        String sqlQuery ="select * from messages;";
+        sqlQuery += (messageId > 0) ? " where messageid=" + messageId + " order by messageid;" : " order by message;";
+        Statement st = createStatement();
+        ResultSet rows = st.executeQuery(sqlQuery);
+        while (rows.next())
+        {
+            logger.log(Level.INFO, "Reading row...");
+            Message msg = new Message();
+            msg.setMessageId(rows.getInt("messageid"));
+            msg.setUserId(rows.getInt("userid"));
+            msg.setMessage(rows.getString("message"));
+            msg.setDateadded(rows.getDate("dateadded"));
+            logger.log(Level.INFO, "Adding message to list with id=" + msg.getMessageId());
+            ll.add(msg);
+        }
+        logger.log(Level.INFO, "Done reading messages data...");
+        return ll.toArray(new Message[ll.size()]);
+    }
+   
+    public void deleteMessage(int messageid) throws SQLException
+    {
+        String sqlDelete="delete from messages where messageid=?";
+        PreparedStatement pst = createPreparedStatement(sqlDelete);
+        pst.setInt(1, messageid);
+        pst.execute();
+    }
+    
+    public boolean updateMessage(Message msg) throws SQLException
+    {
+        StringBuilder sqlQuery = new StringBuilder();
+        sqlQuery.append("update messages ");
+        sqlQuery.append("set userid=" + msg.getUserId()+ ", ");
+        sqlQuery.append("message='" + msg.getMessage()+ "', ");
+        sqlQuery.append("dateadded='" + msg.getDateadded()+ "' ");
+        sqlQuery.append("where messageid=" + msg.getMessageId() + ";");
+        Statement st = createStatement();
+        logger.log(Level.INFO, "UPDATE SQL=" + sqlQuery.toString());
+        return st.execute(sqlQuery.toString());
+    }
+    
+    public int newMessage(Message msg) throws SQLException
+    {
+        String sqlInsert = (msg.getDateadded() != null ?
+                "insert into messages (userid, message, dateadded) values (" + msg.getUserId() + ",'" + msg.getMessage()+ "','" + msg.getDateadded().toString() + "');" :
+                "insert into messages (userid, message, dateadded) values (" + msg.getUserId() + ",'" + msg.getMessage()+ "',now());");
+                
+        Statement s = createStatement();
+        logger.log(Level.INFO, "attempting statement execute");
+        s.execute(sqlInsert,Statement.RETURN_GENERATED_KEYS);
+        logger.log(Level.INFO, "statement executed.  atempting get generated keys");
+        ResultSet rs = s.getGeneratedKeys();
+        logger.log(Level.INFO, "retrieved keys from statement");
+        int msgid = -1;
+        while (rs.next())
+            msgid = rs.getInt(1);   // assuming 1st column is msgid
+        logger.log(Level.INFO, "The new msg id=" + msgid);
+        return msgid;
+    }
+}
